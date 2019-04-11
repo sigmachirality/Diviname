e640a0e9e2b35ec92f2530a2a9cd13b59f0fccd3
Daniel Tao <danieltaox@gmail.com>
2019-04-10 21:49:29 -0400

    Fix data scraper CSV output

--- data_collection/scraper.py
+++ data_collection/scraper.py
@@ -4,24 +4,23 @@ import numpy as np
 from requests import get
 import os
 
+# Function that extracts sentences from a name analysis page on the Kabalarians website
 def get_sentences(url):
     page = get(url)
     soup = bs(page.text, 'html.parser')
-    return ([sentence.text for sentence in soup.select('#headerOL li')[1:]])
+    return ([sentence.text.replace("Â", " ") for sentence in soup.select('#headerOL li')[1:]])
 
-
+# Extract sentences from 3000 randomly selected names.
 name_website_root = "https://www.kabalarians.com/name-meanings/names"
 name_df = pd.read_csv(os.path.join(os.getcwd(), 'name_gender.csv'))
-
 sentences = set()
 f = lambda gender: "/male/" if gender == "M" else "/female/"
 for _, name in name_df.sample(n=3000).iterrows():
     path = name_website_root + f(name["gender"]) + name["name"] + ".htm"
-    print(path)
-    set.update(set(get_sentences(path)))
+    sentences.update(set(get_sentences(path)))
 
-output = pd.Series(list(sentences))
-output.to_csv("sentences.csv")
+# Save scraped sentences as a csv.
+pd.Series(list(sentences)).to_csv("sentences.csv")
 
 
 
