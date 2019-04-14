cf25e0964d1986a8bc7b18596700b7e1bfb71b58
Daniel Tao <danieltaox@gmail.com>
2019-04-14 02:39:48 -0400

    Fix sentences formatting

--- data_collection/scraper.py
+++ data_collection/scraper.py
@@ -9,14 +9,14 @@ import os
 def get_sentences(url):
     page = get(url)
     soup = bs(page.text, 'html.parser')
-    return ([sentence.text.replace("Â", " ") for sentence in soup.select('#headerOL ul li')[1:]])
+    return[sentence.text.partition(u'\xa0')[0].strip() for sentence in soup.select('#headerOL > ul li')[1:]])
 
 # Extract sentences from 3000 randomly selected names.
 name_website_root = "https://www.kabalarians.com/name-meanings/names"
 name_df = pd.read_csv(os.path.join(os.getcwd(), 'name_gender.csv'))
 sentences = set()
 f = lambda gender: "/male/" if gender == "M" else "/female/"
-for _, name in name_df.sample(n=3000).iterrows():
+for _, name in name_df.sample(n=1).iterrows():
     path = name_website_root + f(name["gender"]) + name["name"] + ".htm"
     sentences.update(set(get_sentences(path)))
 
